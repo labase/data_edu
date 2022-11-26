be3557ef0547ac785715d262b2f9a598176b6257
Carlo E. T. Oliveira <carlo@nce.ufrj.br>
2022-11-26 09:38:22 -0300

    Saved kwarworker/_code/master_ola.py @2022-11-26 09:38

--- kwarworker/_code/master_ola.py
+++ kwarworker/_code/master_ola.py
@@ -15,6 +15,7 @@ class Ola(Suucury): #(suucury.Suucury):
 
         await self.leste()
         await self.leste()
+        await self.s()
         await self.leste()
         await self.leste()
         await self.leste()
